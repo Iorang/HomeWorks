90244a46610d3a9a78d3d5d5ffc49a9afbdc76ee
petrikovi <pokaiparga@gmail.com>
2025-03-06 12:33:02 +0500

    HW4

---
+++ Task4_4/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace Task4_4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double a;
+            double b;
+            double result;
+
+            Console.WriteLine("Введите число a:");
+            a = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Введите число b:");
+            b = Convert.ToDouble(Console.ReadLine());
+
+            result = a;
+
+            for (int i = 0; i >= b; i--)
+            {
+                result *= 1.0/a;
+            }
+
+            Console.WriteLine($"{a}^{b} = " + result);
+            Console.ReadLine();
+        }
+    }
+}
+
